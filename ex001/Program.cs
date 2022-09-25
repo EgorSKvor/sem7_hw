8eb0da1d3a11baae0cfae925714ac3e03bc4b851
Egor <ee3068316@gmail.com>
2022-09-25 14:17:58 +0300

    add ex1

--- ex001/Program.cs
+++ ex001/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double[,] matrix = new double[3,4];
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i, j] = new Random().Next(-10, 10);
+        System.Console.Write(matrix[i,j] + " ");
+    }
+    System.Console.WriteLine();
+}
